9a555b7a294404dc48f5c8c1c32a47262bfa9c42
TheUncertainKing <izaakrawlinson@gmail.com>
2025-09-16 06:00:48 -0600

    lots of work
    shuffling

--- scum.js
+++ scum.js
@@ -1,17 +1,42 @@
-let deck = [0]
+//variables
 
-//adds cards to deck
-for (let c = 1; c < 108; c++){
-    deck.push = c
-    console.log(c)
+let player = {
+    name: "User",
+    isKing: false,
+    isScum: false,
+    hand: [0]
 }
 
+let drawTest = document.getElementById("draw-test")
+
+var deck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108]
+
+
+//draw card [working]
 function drawCard() {
+    if (deck.length <= 0) {
+        drawTest.textContent += "Empty"
+    } else {
    let randomCard = Math.floor(Math.random() * deck.length)
-   console.log(randomCard)
+   drawTest.textContent += deck[randomCard] + " "
+   deck.splice(randomCard, 1)
+    }
 }
 
-drawCard()
- //deck.splice(deck.indexOf(1), 1)
 
- 
+//initial deal [working]
+function dealer() {
+    if (deck.length <= 0) {
+        drawTest.textContent += "Empty"
+    } else {
+    for (let c = 1; c < 14; c++) {
+    let randomCard = Math.floor(Math.random() * deck.length)
+    drawTest.textContent += deck[randomCard] + " "
+    deck.splice(randomCard, 1)
+        }
+    }
+}
+
+
+
+
